a4d15ac3d55c00ba94da9869a9362aa383e371e4
Carlos Solano <solanoandy@gmail.com>
2022-01-14 12:29:00 -0600

    finished setting up useLocalStorage, useform, and checkout form

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,20 @@
+import { useState } from "React";
+
+export const useLocalStorage = (key, initialValues) =>
+{
+    // set and update localStorage and also retrieves that value if the user refreshes or closes the window
+    const [values, setValues] =useState(() =>
+    {
+        const item = window.localStorage.getItem(key);
+
+        return item ? JSON.parse(item) : localStorage.setItem(key, JSON.stringify(initialValues));
+    });
+
+    const setStoredValues = (values) =>
+    {
+        localStorage.setItem(key, JSON.stringify(values));
+        setValues(values);
+    };
+
+    return [values, setStoredValues];
+};
